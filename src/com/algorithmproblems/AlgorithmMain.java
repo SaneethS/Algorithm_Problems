f95bea80d4ee4d12f1ce61e055ed165ac64781c8
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-22 18:25:33 +0530

    Permutation String problem is completed

--- src/com/algorithmproblems/AlgorithmMain.java
+++ src/com/algorithmproblems/AlgorithmMain.java
@@ -4,6 +4,7 @@ public class AlgorithmMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Algorithm problems");
+		StringPermutation.stringPermutation();
 //		BinarySearch.run();
 //		BubbleSort.runBubbleSort();
 //		InsertionSort.runInsertionSort();
@@ -12,6 +13,7 @@ public class AlgorithmMain {
 //		PrimeNumber.runPrimeNumber();
 //		PrimeNumberExtended.runPrimeNumberExtended();
 //		Generics.runGenerics();
-		FindYourNumber.findYourNumber(Integer.parseInt(args[0]));
+//		FindYourNumber.findYourNumber(Integer.parseInt(args[0]));
+		
 	}
 }
